4c0beb3d5cfa1661fd8e9d4ec20503c8514a2584
bipinnewar <bipinshrestha2058@gmail.com>
2023-11-27 13:49:23 +0545

    main

---
+++ TaskProject/tasks/urls.py
@@ -0,0 +1,25 @@
+# tasks/urls.py
+
+from django.urls import path, re_path
+from . import views
+
+# namespace
+app_name = 'tasks'
+
+urlpatterns = [
+
+    # Create a task
+    path('create/', views.task_create, name='task_create'),
+
+    # Retrieve task list
+    path('', views.task_list, name='task_list'),
+
+   # Retrieve single task object
+    path('<int:pk>/', views.task_detail, name='task_detail'),
+
+    # Update a task
+    path('<int:pk>/update/', views.task_update, name='task_update'),
+
+    # Delete a task
+    path('<int:pk>/delete/', views.task_delete, name='task_delete'),
+]
